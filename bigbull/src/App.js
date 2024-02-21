b61e250ebe8f2bc60a98e0b6c8c49dfef73c95b7
vaibhavpatil4744 <vaibhavbpatil123@gmail.com>
2024-02-21 20:27:45 +0530

    new codes are  added

--- bigbull/src/App.js
+++ bigbull/src/App.js
@@ -1,10 +1,31 @@
-import React from 'react'
-
-const App = () => {
+import "./App.css"
+import Header from "./components/common/header/Header"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import About from "./components/about/About"
+import CourseHome from "./components/allcourses/CourseHome"
+import Team from "./components/team/Team"
+import Pricing from "./components/pricing/Pricing"
+import Blog from "./components/blog/Blog"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/common/footer/Footer"
+import Home from "./components/home/Home"
+function App() {
   return (
-    <div>
-      <h1>vaibhav</h1>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/courses' component={CourseHome} />
+          <Route exact path='/team' component={Team} />
+          <Route exact path='/pricing' component={Pricing} />
+          <Route exact path='/journal' component={Blog} />
+          <Route exact path='/contact' component={Contact} />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   )
 }
 
